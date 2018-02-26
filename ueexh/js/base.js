99d5712c4cfbdef0be4696b876fbeb8615031b67
ch41618@ly.com <ch41618@ly.com>
2018-02-26 15:04:30 +0800

    提交 Signed-off-by: ch41618@ly.com <ch41618@ly.com>

---
+++ ueexh/js/base.js
@@ -0,0 +1,21 @@
+function jump(num){
+	if(num==1){
+		location.href="index.html"
+	}else if(num==2){
+		location.href="jiaoyi.html"
+		
+	}else if(num==3){
+		location.href="../zichanlist/chongzhi.html"
+		
+	}else if(num==4){
+		location.href="../zichanlist/safeinfo.html"
+		
+	}else if(num==5){
+		location.href="../gonggao/gonggao.html"
+		
+	}else if(num==8){
+		location.href="../zhuce/login.html"
+	}else if(num==9){
+		location.href="../zhuce/zhuce.html"
+	}
+}
