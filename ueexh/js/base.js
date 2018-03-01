6603beaa7b422582117600191db1fc27177659fb
ch41618@ly.com <ch41618@ly.com>
2018-03-01 17:55:27 +0800

    tijiao

--- ueexh/js/base.js
+++ ueexh/js/base.js
@@ -19,3 +19,24 @@ function jump(num){
 		location.href="../zhuce/zhuce.html"
 	}
 }
+function footerjump(num){
+	if(num==1){
+		location.href="index.html"
+	}else if(num==2){
+		location.href="../gonggao/shenfenrz.html"
+		
+	}else if(num==3){
+		location.href="../gonggao/tijiaogd.html"
+		
+	}else if(num==4){
+		location.href="../falv/yhxieyi.html"
+		
+	}else if(num==5){
+		location.href="../falv/yinsi.html"
+		
+	}else if(num==8){
+		location.href="../falv/falv.html"
+	}else if(num==9){
+		location.href="../zhuce/zhuce.html"
+	}
+}
